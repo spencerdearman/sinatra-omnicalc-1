c9e3a8a0332d0a7e605e446eed3c5683f22eb138
Spencer Dearman <121642493+spencerdearman@users.noreply.github.com>
2024-10-22 03:39:58 +0000

    FIRST TRY BABY

--- app.rb
+++ app.rb
@@ -2,8 +2,56 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get('/square/new') do
+  erb(:square)
+end
+
+get('/square/results') do
+  @number = params[:square].to_f
+  @square = @number * @number
+  erb(:square_results)
+end
+
+get('/square_root/new') do
+  erb(:sqrt)
+end
+
+get('/square_root/results') do
+  @number = params[:user_number].to_f
+  @square_root = Math.sqrt(@number)
+  erb(:sqrt_results)
+end
+
+get('/payment/new') do
+  erb(:payment)
+end
+
+get('/payment/results') do
+  # Extract and convert input parameters
+  @apr = params[:user_apr].to_f
+  @years = params[:user_years].to_i
+  @principle = params[:user_pv].to_f
+  @monthly_rate = (@apr / 100) / 12
+
+  @num_months = @years * 12
+
+  @numerator = @monthly_rate * @principle
+  @denominator = 1 - (1 + @monthly_rate)**-@num_months
+  @monthly_payment = @numerator / @denominator
+
+  erb(:payment_results)
+end
+
+get('/random/new') do
+  erb(:random)
+end
+
+get('/random/results') do
+  @min = params[:user_min].to_f
+  @max = params[:user_max].to_f
+  @num = rand(@min..@max)
+  erb(:random_results)
 end
